1a4af186e961c93f30266bf64f1263d2d2dc9e96
Alan Higginson <ahigginson@adga.ca>
2019-10-20 01:46:19 -0400

    add extended high filters

---
+++ src/components/BeamModes/ExtendedHigh.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="bm_extendedhigh">
+    <v-container fluid>
+      <v-checkbox @click.native="handleClick" dense v-model="selected" label="Extended High 1" value="Extended High 1"></v-checkbox>
+      <v-checkbox @click.native="handleClick" dense v-model="selected" label="Extended High 2" value="Extended High 2"></v-checkbox>
+      <v-checkbox @click.native="handleClick" dense v-model="selected" label="Extended High 3" value="Extended High 3"></v-checkbox>
+      <v-checkbox @click.native="handleClick" dense v-model="selected" label="Extended High 4" value="Extended High 4"></v-checkbox>
+      <v-checkbox @click.native="handleClick" dense v-model="selected" label="Extended High 5" value="Extended High 5"></v-checkbox>
+      <v-checkbox @click.native="handleClick" dense v-model="selected" label="Extended High 6" value="Extended High 6"></v-checkbox>
+    </v-container>
+    <v-btn @click="toggleAll">{{toggleButtonName}}</v-btn>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'BM_ExtendedHigh',
+  components: {
+  },
+  data () {
+    return {
+      selected: this.$store.state.filterState.ExtendedHigh,
+      toggleButtonName: "Select All",
+    }
+  },
+  methods: {
+    handleClick: function() {
+      let filters = this.$store.state.filterState;
+      filters.ExtendedHigh = this.selected
+      this.$store.commit('assignFilters', filters)
+      this.refreshMap();
+    },
+      toggleAll: function() {
+      if(this.selected.length == 6) {
+        this.selected = [];
+        this.toggleButtonName = "Select All";
+      } else {
+        this.selected = ['Extended High 1', 'Extended High 2', 'Extended High 3', 'Extended High 4', 'Extended High 5', 'Extended High 6'];
+        this.toggleButtonName = "Select None";
+      }
+      let filters = this.$store.state.filterState;
+      filters.ExtendedHigh = this.selected
+      this.$store.commit('assignFilters', filters)
+      this.refreshMap();
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
--- src/components/FilterBar.vue
+++ src/components/FilterBar.vue
@@ -1,100 +1,97 @@
 <template>
-      <v-navigation-drawer
-      v-model="drawer"
-      :clipped="$vuetify.breakpoint.lgAndUp"
-      app
-    >
-      <v-list dense>
-        <template v-for="(item, i) in filters">
-          <!-- <v-layout
+  <v-navigation-drawer v-model="drawer" :clipped="$vuetify.breakpoint.lgAndUp" app>
+    <v-list dense>
+      <template v-for="(item, i) in filters">
+        <!-- <v-layout
             v-if="item.heading"
             :key="item.heading"
             align-center
           >
-          </v-layout> -->
-          <v-list-group
-            :key="item.text"
-            v-model="item.model"
-            :prepend-icon="item.model ? item.icon : item['icon-alt']"
-            append-icon=""
-          >
-            <template v-slot:activator>
-              <v-list-item>
-                <v-list-item-content>
-                  <v-list-item-title>
-                    {{ item.name }}
-                  </v-list-item-title>
-                </v-list-item-content>
-              </v-list-item>
-            </template>
-            <v-list-item-subtitle class="filterSubtitle">{{ item.modes[i].name }}</v-list-item-subtitle>
-            <!-- <p>{{ item.modes[i].name }}</p> -->
-            <v-list-item
-              v-for="mode in item.modes"
-              :key="mode.name"
-            >
-              <v-list-item-action v-if="mode.icon">
-                <v-icon>{{ mode.icon }}</v-icon>
-              </v-list-item-action>
+        </v-layout>-->
+        <v-list-group
+          :key="item.text"
+          v-model="item.model"
+          :prepend-icon="item.model ? item.icon : item['icon-alt']"
+          append-icon
+        >
+          <template v-slot:activator>
+            <v-list-item>
               <v-list-item-content>
-                <component v-bind:is="mode.componentName"></component>
+                <v-list-item-title>{{ item.name }}</v-list-item-title>
               </v-list-item-content>
             </v-list-item>
-          </v-list-group>
-        </template>
-      </v-list>
-    </v-navigation-drawer>
+          </template>
+          <v-list-item-subtitle class="filterSubtitle">{{ item.modes[i].name }}</v-list-item-subtitle>
+          <!-- <p>{{ item.modes[i].name }}</p> -->
+          <v-list-item v-for="mode in item.modes" :key="mode.name">
+            <v-list-item-action v-if="mode.icon">
+              <v-icon>{{ mode.icon }}</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <component v-bind:is="mode.componentName"></component>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-group>
+      </template>
+    </v-list>
+  </v-navigation-drawer>
 </template>
 
 <script>
-import BMStandard from '@/components/BeamModes/Standard.vue';
-import BMFine from '@/components/BeamModes/Fine.vue';
-import BMScn from '@/components/BeamModes/ScanSarNarrow.vue';
+import BMStandard from "@/components/BeamModes/Standard.vue";
+import BMFine from "@/components/BeamModes/Fine.vue";
+import BMScn from "@/components/BeamModes/ScanSarNarrow.vue";
+import BMExtendedHigh from "@/components/BeamModes/ExtendedHigh.vue";
 
 export default {
-  name: 'FilterBar',
+  name: "FilterBar",
   props: {
-    source: String,
+    source: String
   },
   components: {
     BMStandard,
     BMFine,
     BMScn,
+    BMExtendedHigh
   },
   data: () => ({
-      drawer: null,
-      filters: [
-        {
-          name: 'Beam Modes',
-          model: true,
-          modes: [
-            {
-              name: 'ScanSAR Narrow',
-              componentName: 'BMScn'
-            },            {
-              name: 'Standard',
-              componentName: 'BMStandard'
-            },
-            {
-              name: 'Fine',
-              componentName: 'BMFine'
-            },
-          ],
-        },
-        // {
-        //   text: 'Date Ranges',
-        //   model: false,
-        //   componentName: 'DatePicker'
-        // },
-      ],
-  }),
+    drawer: null,
+    filters: [
+      {
+        name: "Beam Modes",
+        model: true,
+        modes: [
+          {
+            name: "ScanSAR Narrow",
+            componentName: "BMScn"
+          },
+          {
+            name: "Extended High",
+            componentName: "BMExtendedHigh"
+          },
+          {
+            name: "Standard",
+            componentName: "BMStandard"
+          },
+          {
+            name: "Fine",
+            componentName: "BMFine"
+          }
+        ]
+      }
+      // {
+      //   text: 'Date Ranges',
+      //   model: false,
+      //   componentName: 'DatePicker'
+      // },
+    ]
+  })
 };
-
 </script>
 
 <style scoped>
-  .filterSubtitle {
-    padding-left: 35px;
-    color: black;
-  }
+.filterSubtitle {
+  padding-left: 35px;
+  color: black;
+}
 </style>
