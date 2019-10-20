bd026c45a3af329dccfad5c1e2e0a47ef31d74a2
Alan Higginson <ahigginson@adga.ca>
2019-10-19 21:57:09 -0400

    catch map initialization event and store map object into the store.state

--- src/store.js
+++ src/store.js
@@ -6,13 +6,18 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     drawer: true,
+    the_map: {},
   },
   mutations: {
     toggleDrawer(state) {
       state.drawer ? state.drawer = false : state.drawer = true;
+    },
+    assignMap(state, map) {
+      state.the_map = map;
     }
+
   },
   actions: {
 
-  }
+}
 })
