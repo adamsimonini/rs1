648253bb87fa25e47d10aea803296c8883a2255b
Adam Simonini <adamsimonini@users.noreply.github.com>
2019-11-02 15:08:30 -0400

    Turn off eslint "no-console" warning
    done in vue.config.js (as a result of using vue cli)
    now only on in production. we should turn it back on for dev when production-ready

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack')
  
 module.exports = {
+  lintOnSave: process.env.NODE_ENV !== 'development',
   configureWebpack: {
     plugins: [
       new webpack.ProvidePlugin({
